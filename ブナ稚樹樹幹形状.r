4d2f98eace056689a2f15ad419d4f418c2557135
yutaro-shimizu <68834841+yutaro-shimizu@users.noreply.github.com>
2022-06-17 02:23:55 +0900

    changed glm() to lm() with commented out bootstrap

--- ブナ稚樹樹幹形状.r
+++ ブナ稚樹樹幹形状.r
@@ -1,10 +1,25 @@
-d <- read.csv("R_Beech_Roots.csv")
+install.packages("psych")
+install.packages("MuMIn")
+# install.packages("arm") bootstrap
+install.packages("performanceAnalytics")
+
+d <- R_Beech_Roots
 attach(d)
 names(d)
 
-#多重共線性の把握（相関係数0.7以上の説明変数対は除外する
+#多重共線性の把握（相関係数0.7以上の説明変数対は除外する）
 library(psych)
 cor(d[3:21], use="pairwise.complete.obs", method="p") 
+
+#共線性の可視化と線形性の確認
+library(PerformanceAnalytics)
+vars <- c("Height",
+          "T.R",
+          "buried_trunk_.",
+          "buried_trunk_weight",
+          "buried_trunk_years")
+new_data <- R_Beech_Roots[vars]
+chart.Correlation(new_data)
 
 ######計測項目######
 #環境パラメータ：
@@ -38,87 +53,105 @@ cor(d[3:21], use="pairwise.complete.obs", method="p")
 
 #用語の参考論文：chrome-extension://oemmndcbldboiebfnladdacbdfmadadm/https://www.jstage.jst.go.jp/article/jjfs1953/76/1/76_1_18/_pdf
 
+
 #GLM、ガンマ分布
-model <- glm(
-	Height　#応答変数は樹高（地上部の垂直伸長が森林構成種としての基盤）
-	~L.H	#地上部の長さ/樹高
-	+root.width	#根の斜面水平方向の伸張幅
-	+T.R		　　#T/R比
-	+buried_trunk_.　#埋幹部の重量割合
-	+buried_trunk_weight　#埋幹部の重量(root_weightと高い相関)
-	+buried_trunk_years　#每幹部の年数
-	+trunk_inclination　#主幹の傾斜角
-	+slope　　　#斜面傾斜　
-	+snow_depth,　#平均最深積雪
-	Gamma)
+# model <- glm(Height #応答変数は樹高（地上部の垂直伸長が森林構成種としての基盤）
+# ~L.H	#地上部の長さ/樹高
+# +root.width	#根の斜面水平方向の伸張幅
+# +T.R		　　#T/R比 grams ratio
+# +buried_trunk_.　#埋幹部の重量割合
+# +buried_trunk_weight　#埋幹部の重量(root_weightと高い相関)
+# +buried_trunk_years　#每幹部の年数
+# +trunk_inclination　#主幹の傾斜角
+# +slope　　　#斜面傾斜
+# +snow_depth,　#平均最深積雪
+# Gamma)
 
 #説明変数：多重共線性の把握から、LH比、、TR比、
-#節目変数つづき：タップルートの重量割合、タップルートの重量、タップルートの年数、斜面傾斜、サイトの平均最大積雪深
+#説明変数つづき：タップルートの重量割合、タップルートの重量、タップルートの年数、斜面傾斜、サイトの平均最大積雪深
 #Γ分布のGLM
+
+#LM、重回帰分析
+model <- lm(Height~
+              L.H 
+            +root.width
+            +T.R
+            +buried_trunk_.
+            +buried_trunk_weight
+            +buried_trunk_years 
+            +trunk_inclination
+            +slope
+            +snow_depth,
+            data=R_Beech_Roots)
 
 library(MuMIn)
 options(na.action = "na.fail")
-dredge(model, rank="AIC") 
-
-#AIC＜2のモデルを有効とする
+dd <- dredge(model, rank="AIC") 
 
 #最適モデル
-summary(
-    glm(Height
-	 ~ root.width
-	 + T.R
-	 + buried_trunk_.
-	 + buried_trunk_years, 
-  Gamma))　
+bestmodel <- get.models(dd, 1)[[1]]
+AIC(bestmodel) #188.9255
 
-#2ndモデル
-summary(
-  glm(Height
-      ~ root.width
-      + T.R
-      + buried_trunk_.
-      + buried_trunk_years
-      + slope, 
-      Gamma))　
+summary(bestmodel)
+plot(bestmodel) ##qqplot（誤差の等分散正規分布）の確認
 
-#3rdモデル
-summary(
-  glm(Height
-      ~ root.width
-      + T.R
-      + buried_trunk_.
-      + buried_trunk_years
-      + snow_depth, 
-      Gamma))　
-
-#4thモデル
-summary(
-  glm(Height
-      ~ root.width
-      + T.R
-      + buried_trunk_.
-      + buried_trunk_years
-      + trunk_inclination, 
-      Gamma))　
-
-#5thモデル
-summary(
-  glm(Height
-      ~ root.width
-      + T.R
-      + buried_trunk_.
-      + buried_trunk_years
-      + buried_trunk_weight,
-      Gamma))　
-
-#6thモデル
-summary(
-  glm(Height
-      ~ root.width
-      + T.R
-      + buried_trunk_.
-      + buried_trunk_years
-      + L.H,
-      Gamma))　
+# # bootstrap
+# sim(bestmodel,n.sims=100)
+# quantile(sim.lm@coef[,3], probs = c(0.025, 0.975)) #burried_trunk_weight
+# quantile(sim.lm@coef[,5], probs = c(0.025, 0.975)) #T.R
 
 
+# #AIC＜2のモデルを有効とする
+# 
+# #最適モデル
+# summary(glm(Height~ root.width+T.R+ buried_trunk_.+ buried_trunk_years,Gamma))
+# 
+# #2ndモデル
+# summary(
+#   glm(Height
+#       ~ root.width
+#       + T.R
+#       + buried_trunk_.
+#       + buried_trunk_years
+#       + slope, 
+#       Gamma))　
+# 
+# #3rdモデル
+# summary(
+#   glm(Height
+#       ~ root.width
+#       + T.R
+#       + buried_trunk_.
+#       + buried_trunk_years
+#       + snow_depth, 
+#       Gamma))　
+# 
+# #4thモデル
+# summary(
+#   glm(Height
+#       ~ root.width
+#       + T.R
+#       + buried_trunk_.
+#       + buried_trunk_years
+#       + trunk_inclination, 
+#       Gamma))　
+# 
+# #5thモデル
+# summary(
+#   glm(Height
+#       ~ root.width
+#       + T.R
+#       + buried_trunk_.
+#       + buried_trunk_years
+#       + buried_trunk_weight,
+#       Gamma))　
+# 
+# #6thモデル
+# summary(
+#   glm(Height
+#       ~ root.width
+#       + T.R
+#       + buried_trunk_.
+#       + buried_trunk_years
+#       + L.H,
+#       Gamma))　
